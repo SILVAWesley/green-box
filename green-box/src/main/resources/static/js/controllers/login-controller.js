2f852cffd6405e2c2da364e45372b946317ff969
Natan7 <natan.ribeiro@ccc.ufcg.edu.br>
2016-08-24 01:48:35 -0300

    Adition of information for users, also also adding patterns to register

--- green-box/src/main/resources/static/js/controllers/login-controller.js
+++ green-box/src/main/resources/static/js/controllers/login-controller.js
@@ -15,7 +15,8 @@ function($scope, $http, $state, authService, $localStorage) {
 			if (result) {
 				$state.go('dashboard.directories', {folderPath: $localStorage.session.currentPath});
 			} else {
-				window.alert("Login not successful");
+			    $("#modalLogin").modal("show");
+				//window.alert("Login not successful");
 			}	
 		});
 	}
