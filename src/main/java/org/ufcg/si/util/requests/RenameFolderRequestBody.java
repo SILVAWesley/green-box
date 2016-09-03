6c760cd0378dd494d7285dfeddc5e23316616878
Benardi <jose.nunes@ccc.ufcg.edu.br>
2016-09-02 21:50:36 -0300

    Exclusion of directory green-box

---
+++ src/main/java/org/ufcg/si/util/requests/RenameFolderRequestBody.java
@@ -0,0 +1,56 @@
+package org.ufcg.si.util.requests;
+
+import org.ufcg.si.models.User;
+
+public class RenameFolderRequestBody {
+	private User user;
+	private String newName;
+	private String oldName;
+	private String folderPath;
+	
+	public RenameFolderRequestBody(User user, String newName, String oldName, String folderPath){
+		this.user = user;
+		this.newName = newName;
+		this.oldName = oldName;
+		this.folderPath = folderPath;
+	}
+	
+	public RenameFolderRequestBody(){
+		
+	}
+	
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public String getNewName() {
+		return newName;
+	}
+
+	public void setNewName(String newName) {
+		this.newName = newName;
+	}
+
+	public String getOldName() {
+		return oldName;
+	}
+
+	public void setOldName(String oldName) {
+		this.oldName = oldName;
+	}
+
+	public String getFolderPath() {
+		return folderPath;
+	}
+
+	public void setFolderPath(String newFolderPath) {
+		this.folderPath = newFolderPath;
+	}
+	
+	
+	
+}
